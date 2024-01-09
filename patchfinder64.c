0c47502e76fa4e731e51bd05566e3adb2a1da7d4
wh1te4ever <88495487+wh1te4ever@users.noreply.github.com>
2024-01-09 23:39:31 +0900

    Working: gPhysBase offset

--- patchfinder64.c
+++ patchfinder64.c
@@ -1005,7 +1005,7 @@ addr_t find_cdevsw(void)
         0x54000aa1
     };
     
-    uint64_t addr = (uint64_t)boyermoore_horspool_memmem((unsigned char *)((uint64_t)kernel + prelink_base), prelink_size, (const unsigned char *)bytes, sizeof(bytes));
+    uint64_t addr = (uint64_t)boyermoore_horspool_memmem((unsigned char *)((uint64_t)kernel + xnucore_base), xnucore_size, (const unsigned char *)bytes, sizeof(bytes));
     
     if (!addr) {
         return 0;
@@ -1025,6 +1025,36 @@ addr_t find_cdevsw(void)
     }
     //4. Get label from adrl opcode.
     addr = follow_adrl(kernel, addr);
+    
+    return addr + kerndumpbase;
+}
+
+addr_t find_gPhysBase(void)
+{
+    //_invalidate_icache64
+    //1. Find opcode (5F 00 00 71 20 01 00 54 [ADRL 8 bytes] 42 00 40 F9 00 00 02 CB)
+    bool found = false;
+    uint64_t addr = 0;
+    addr_t off;
+    uint32_t *k;
+    k = (uint32_t *)(kernel + xnucore_base);
+    for (off = 0; off < xnucore_size - 4; off += 4, k++) {
+        if (k[0] == 0x7100005F
+            && k[1] == 0x54000120
+            && (k[2] & 0x9F000000) == 0x90000000    //adrp
+            && (k[3] & 0xFF800000) == 0x91000000    //add
+            && k[4]== 0xF9400042
+            && k[5] == 0xCB020000) {
+//            return off + xnucore_base + kerndumpbase;
+            addr = off + xnucore_base;
+            found = true;
+        }
+    }
+    if(!found)
+        return 0;
+    
+    //2. Get label from [ADRL 8 bytes]
+    addr = follow_adrl(kernel, addr + 8);
     
     return addr + kerndumpbase;
 }
@@ -1067,6 +1097,7 @@ main(int argc, char **argv)
 } while(false)
     
     CHECK(cdevsw);
+    CHECK(gPhysBase);
     
     
     term_kernel();
